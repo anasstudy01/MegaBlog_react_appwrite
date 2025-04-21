6b1627ccbc6245d5c00ea70b9af79498a0da2155
anas <anasstudy01@gmail.com>
2025-04-22 00:58:37 +0530

    fix: correct destructuring of loading state in App component

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import { Outlet } from 'react-router-dom';
 function App() {
  
 
-  const {loading ,setLoading} = useState(true) ;
+const [loading,setLoading] = useState(true);
 const dispatch = useDispatch();
 
 useEffect(() => {
